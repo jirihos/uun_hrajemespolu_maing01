6c411def65e7dad058936d032b450f882ad1e873
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2023-12-10 10:06:28 +0100

    Meziverze s filter a .Dialog

--- uun_hrajemespolu_maing01-hi/src/bricks/own-reservation-list/view.js
+++ uun_hrajemespolu_maing01-hi/src/bricks/own-reservation-list/view.js
@@ -45,11 +45,20 @@ const View = createVisualComponent({
   render(props) {
     //@@viewOn:private
     const { dataObject } = props;
+    const [open, setOpen] = useState(false);
     const { state, data } = dataObject;
-    
+    const [filter, setFilter] = useState(true);
+    const filteredData = Array.isArray(dataObject.data)
+         ? dataObject.data.filter(
+        itemData =>
+         itemData.data.state === "valid" || filter === false
+    )
+     : [];
 
+    console.log("filteredData", filteredData);
     console.log("dataObject", dataObject);
     console.log("dataObject.data", dataObject.data);
+    console.log("filter", filter);
 
     //@@viewOff:private
 
@@ -58,21 +67,23 @@ const View = createVisualComponent({
     const currentNestingLevel = Utils.NestingLevel.getNestingLevel(props, View);
 
     return currentNestingLevel ? (
+
       <>
       <div className="center">
-      <h1>List of reservations</h1>
+      <h1>List rezervací</h1>
           <Uu5Elements.Header
           />
-          <Uu5Elements.Toggle  // todo funkcionalita toggle
-                value={1}
-                onChange={(e) => setValue(e.data.value)}
-                Toggle label="Aktivní/ Archivní rezervace"
+           <Uu5Elements.Toggle
+            value={filter}
+            onChange={(e) => setFilter(e.data.value)}
+            label={filter ? 'Pouze aktivní rezervace' : 'Všechny rezervace'}
+            {...props}
           />
       </div>        
         {(state === "pending" || state === "pendingNoData") && <Uu5Elements.Pending />}
         {(state === "error" || state === "errorNoData" || state === "readyNoData") && <h1>Error</h1>}
         {state === "ready" && (
-          dataObject.data.map((itemData, index) => (
+          filteredData.map((itemData, index) => (
             
             <Uu5Elements.Block
               card="full"
@@ -82,31 +93,25 @@ const View = createVisualComponent({
               key={itemData.data.id} 
               header={
                 <Uu5Elements.Text category="story" segment="heading" type="h5">
-                  Rezervace uživatele: {itemData.data.uuIdentity} Pro sportoviště: {itemData.data.sportsFieldId}
+                  Rezervace sportoviště: {itemData.data.sportsFieldId} 
                 </Uu5Elements.Text>
               }
               footer={
                 <Uu5Elements.Grid>
-                <Uu5Elements.Grid.Item  rowSpan={2}>
-                  <Uu5Elements.Box className={Config.Css.css({ padding: 4})}>Rezervace sportoviště: {itemData.data.sportsFieldId}</Uu5Elements.Box>
+                  <Uu5Elements.Grid.Item rowSpan={2}>
+                  <Uu5Elements.Text className={Config.Css.css({  padding: 4 })}>Začíná: {itemData.data.startTs}</Uu5Elements.Text>
                   </Uu5Elements.Grid.Item>
                   <Uu5Elements.Grid.Item rowSpan={2}>
-                  <Uu5Elements.Box className={Config.Css.css({  padding: 4 })}>Začíná: {itemData.data.startTs}</Uu5Elements.Box>
+                  <Uu5Elements.Text className={Config.Css.css({  padding: 4 })}>Končí: {itemData.data.endTs}</Uu5Elements.Text>
                   </Uu5Elements.Grid.Item>
                   <Uu5Elements.Grid.Item rowSpan={2}>
-                  <Uu5Elements.Box className={Config.Css.css({  padding: 4 })}>Končí: {itemData.data.endTs}</Uu5Elements.Box>
+                  <Uu5Elements.Text className={Config.Css.css({  padding: 4 })}>Id rezervace:{itemData.data.id}</Uu5Elements.Text>
                   </Uu5Elements.Grid.Item>
-                  <Uu5Elements.Grid.Item rowSpan={2}>
-                  <Uu5Elements.Box className={Config.Css.css({  padding: 4 })}>Stav: {itemData.data.state}</Uu5Elements.Box>
-                  </Uu5Elements.Grid.Item>
-                  <Uu5Elements.Grid.Item rowSpan={2}>
-                  <Uu5Elements.Box className={Config.Css.css({  padding: 4 })}>Zrušeno z důvodu:{itemData.data.cancelReason}</Uu5Elements.Box>
-                </Uu5Elements.Grid.Item>
               </Uu5Elements.Grid>
               }
             >
               <Uu5Elements.Button 
-              className="center" width={200} effect="upper" onClick={() => console.log("upper")}>
+              className="center" width={200} effect="upper" onClick={() => setOpen(true)}>
                 Zrušit rezervaci
                 </Uu5Elements.Button>
             </Uu5Elements.Block>
@@ -118,7 +123,29 @@ const View = createVisualComponent({
           Načíst další rezervace
           </Uu5Elements.Button> 
         </div>
+        <Uu5Elements.Dialog
+          open={open}
+          onClose={() => setOpen(false)}
+          header="Smazat tuto rezervaci?"
+          icon={<Uu5Elements.Svg code="uugdssvg-svg-delete" />}
+          info="Rezervaci nelze obnovit"
+
+          actionDirection="horizontal"
+          actionList={[
+            {
+              children: "Zrušit"  ,
+              onClick: () => console.log("Cancel"),
+            },
+            {
+              children: "Smazat",
+              onClick: () => console.log("Delete"),
+              colorScheme: "red",
+              significance: "highlighted",
+            },
+          ]}
+        />
       </>
+      
     ) : null;
     //@@viewOff:render
   },
