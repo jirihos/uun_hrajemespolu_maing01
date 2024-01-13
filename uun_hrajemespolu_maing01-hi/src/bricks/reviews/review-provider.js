2f36d2d39e8a123561e9a7537362f3bb4f64e819
Martin Nebehay <martinnebe456@gmail.com>
2024-01-13 12:51:25 +0100

    review-provider.js modified

--- uun_hrajemespolu_maing01-hi/src/bricks/reviews/review-provider.js
+++ uun_hrajemespolu_maing01-hi/src/bricks/reviews/review-provider.js
@@ -1,5 +1,6 @@
 //@@viewOn:imports
-import { createComponent, useDataObject, PropTypes } from "uu5g05";
+import { createComponent, useDataObject, PropTypes, useState } from "uu5g05";
+import { Alert } from "uu5g05-elements";
 import Config from "./config/config.js";
 import Calls from "../../calls.js";
 //@@viewOff:imports
@@ -29,39 +30,57 @@ const ReviewProvider = createComponent({
   render(props) {
     //@@viewOn:private
     const { children, sportsFieldId, uuIdentity } = props;
-    //@@viewOff:private
+
+    const [customError, setCustomError] = useState(null);
 
     const dataObject = useDataObject({
       handlerMap: {
-        getByUser: () => {
-          const dtoIn = {
-            sportsFieldId: sportsFieldId,
-            uuIdentity: uuIdentity,
-          };
+        getByUser: (dtoIn) => {
+          dtoIn.sportsFieldId = sportsFieldId;
+          dtoIn.uuIdentity = uuIdentity;
           return Calls.reviewGetByUser(dtoIn);
         },
-        create: () => {
-          const dtoIn = {
-            sportsFieldId: sportsFieldId,
-            uuIdentity: uuIdentity,
-          };
-          return Calls.reviewCreate(dtoIn);
+        create: async (dtoIn) => {
+          dtoIn.sportsFieldId = sportsFieldId;
+          dtoIn.uuIdentity = uuIdentity;
+
+          let result;
+          try {
+            result = await Calls.reviewCreate(dtoIn);
+          } catch (e) {
+            setCustomError(e.message);
+          }
+          return result;
         },
-        update: () => {
-          const dtoIn = {
-            sportsFieldId: sportsFieldId,
-            uuIdentity: uuIdentity,
-          };
-          return Calls.reviewUpdate(dtoIn);
-        }
+        update: async (dtoIn) => {
+          dtoIn.sportsFieldId = sportsFieldId;
+          dtoIn.uuIdentity = uuIdentity;
+
+          let result;
+          try {
+            result = await Calls.reviewUpdate(dtoIn);
+          } catch (e) {
+            setCustomError(e.message);
+            return null;
+          }
+          return result;
+        },
       },
     });
+    //@@viewOff:private
 
     //@@viewOn:interface
     //@@viewOff:interface
 
     //@@viewOn:render
-    return typeof children === "function" ? children(dataObject) : children;
+    return (
+      <>
+        {typeof children === "function" ? children(dataObject) : children}
+        {customError && (
+          <Alert message={customError} priority="error" durationMs={6000} onClose={() => setCustomError(null)} />
+        )}
+      </>
+    );
     //@@viewOff:render
   },
 });
